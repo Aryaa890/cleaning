f0e71bf9fe7094b7a18906e80009795884dba2d2
Arya890 <arya7ghufron@gmail.com>
2024-05-19 02:24:07 +0700

    Penambahan page login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,6 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('templates/header');
         $this->load->view('home/index');
-        $this->load->view('templates/footer');
     }
 }
